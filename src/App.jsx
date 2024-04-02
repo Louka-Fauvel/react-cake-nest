8e75294c37d462bf43f3d0287a88bcd13488ef0d
Louka-Fauvel <73880235+Louka-Fauvel@users.noreply.github.com>
2024-04-02 16:37:51 +0200

    Fix : organisation pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router";
-import LoginPage from "./components/pages/login/LoginPage";
-import OrderPage from "./components/pages/order/OrderPage";
-import ErrorPage from "./components/pages/error/ErrorPage";
+import LoginPage from "./pages/login/LoginPage";
+import OrderPage from "./pages/order/OrderPage";
+import ErrorPage from "./pages/error/ErrorPage";
 
 export default function App() {
 
--- src/pages/order/OrderPage.jsx
+++ src/pages/order/OrderPage.jsx
@@ -1,5 +1,4 @@
-import { useParams } from "react-router";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export default function OrderPage() {
 
